9a7d94d404601209cd159514d985bb5029aebe8e
Duman Sabyrbayev <90duman@mail.ru>
2023-01-31 23:14:54 +0600

    added view model

--- app/src/main/java/kz/duman/wetherapp/MainActivity.kt
+++ app/src/main/java/kz/duman/wetherapp/MainActivity.kt
@@ -8,6 +8,7 @@ import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kz.duman.wetherapp.databinding.ActivityMainBinding
+import kz.duman.wetherapp.fragments.MainFragment
 import org.json.JSONObject
 
 const val API_KEY = "1fd90f74ae8947ba813105827231701&q"
@@ -20,30 +21,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        resultField = findViewById(R.id.textView)
-        binding.bGet.setOnClickListener {
-            getResult("Almaty")
-        }
+        supportFragmentManager.beginTransaction().replace(R.id.placeHolder, MainFragment.newInstance()).commit()
+//        resultField = findViewById(R.id.textView)
+//        binding.bGet.setOnClickListener {
+//            getResult("Almaty")
+//        }
     }
 
-    private fun getResult(name: String) {
-        val url = "https://api.weatherapi.com/v1/current.json" +
-                "?key=$API_KEY&q=Almaty&aqi=no"
-
-        val queue = Volley.newRequestQueue(this)
-        val stringRequest = StringRequest(
-            Request.Method.GET,
-            url,
-            { response ->
-                val obj  = JSONObject(response)
-                val temp = obj.getJSONObject("current").get("temp_c").toString()
-                resultField.text = temp
-                Log.d("MyLog", "Response: $temp")
-            },
-            {
-                Log.d("MyLog", "Volley error: $it")
-            }
-        )
-        queue.add(stringRequest)
-    }
+//    private fun getResult(name: String) {
+//        val url = "https://api.weatherapi.com/v1/current.json" +
+//                "?key=$API_KEY&q=Almaty&aqi=no"
+//
+//        val queue = Volley.newRequestQueue(this)
+//        val stringRequest = StringRequest(
+//            Request.Method.GET,
+//            url,
+//            { response ->
+//                val obj  = JSONObject(response)
+//                val temp = obj.getJSONObject("current").get("temp_c").toString()
+//                resultField.text = temp
+//                Log.d("MyLog", "Response: $temp")
+//            },
+//            {
+//                Log.d("MyLog", "Volley error: $it")
+//            }
+//        )
+//        queue.add(stringRequest)
+//    }
 }
---
+++ app/src/main/java/kz/duman/wetherapp/MainViewModel.kt
@@ -0,0 +1,9 @@
+package kz.duman.wetherapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+}
